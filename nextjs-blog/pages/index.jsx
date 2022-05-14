c1b4f707dfc372d3f839901c9fc9a5f05b5ea829
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-05-14 20:36:52 +0900

    save

--- nextjs-blog/pages/index.jsx
+++ nextjs-blog/pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,7 +11,11 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          {/* Learn <a href="https://nextjs.org">Next.js!</a> */}
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -49,7 +54,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
+          {/* Powered by <img src="/vercel.svg" alt="Vercel" className="logo" /> */}
+          Powered by <img src="/img/logo.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
