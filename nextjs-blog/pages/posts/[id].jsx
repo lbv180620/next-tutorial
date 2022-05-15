b5f7144d3c9f62e5be187dd9ea3d6d179259fa49
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-05-15 15:41:55 +0900

    save

--- nextjs-blog/pages/posts/[id].jsx
+++ nextjs-blog/pages/posts/[id].jsx
@@ -13,7 +13,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id.join("/")); // rezero/rem
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
